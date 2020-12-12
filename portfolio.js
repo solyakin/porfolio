57224d5e051b86e4eeeda099a80496e9345e6c82
akinlade solomon <solyakin@yahoo.com>
2020-12-12 19:32:19 +0100

    first commit

---
+++ portfolio.js
@@ -0,0 +1,23 @@
+
+//const ScrollReveal = require('scrollreveal')
+//window.sr = ScrollReveal();
+
+ScrollReveal().reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance:'25rem',
+    delay: 300
+});
+
+ScrollReveal().reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+ScrollReveal().reveal('.animate-top', {
+    origin: 'up',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
